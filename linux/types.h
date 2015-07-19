89ab377bd2f5b7513cb14881ef75070867a24508
Lawrence D'Anna <larry@elder-gods.org>
2015-07-19 16:50:08 -0700

    linux __be32 types

--- linux/types.h
+++ linux/types.h
@@ -13,4 +13,13 @@ typedef uint16_t u16;
 typedef uint32_t u32;
 typedef uint64_t u64;
 
+typedef uint16_t __le16;
+typedef uint16_t __be16;
+
+typedef uint32_t __le32;
+typedef uint32_t __be32;
+
+typedef uint64_t __le64;
+typedef uint64_t __be64;
+
 #endif /* _LINUX_TYPES_H */
